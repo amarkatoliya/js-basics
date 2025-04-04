3d2d859df6a18bdd9020a543744b1f3ca2123dff
amarkatoliya <amarbharavad994@gmail.com>
2025-04-04 21:55:06 +0530

    here we add if else basics

---
+++ condition.js/if-else.js
@@ -0,0 +1,11 @@
+function isEligible(age){
+    if(age>= 18){
+        console.log("you are Eligible for voting");
+
+    }else{
+        console.log("you are not Eligible for voting"); 
+    }
+}
+
+isEligible(12);
+isEligible(19);

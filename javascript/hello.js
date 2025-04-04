b49fafc8a7ff6436b0fe2891950dd46cc31b2e54
amarkatoliya <amarbharavad994@gmail.com>
2025-04-04 10:38:40 +0530

    java script basic

---
+++ javascript/hello.js
@@ -0,0 +1,16 @@
+function printHello() {
+    console.log("here is chai")
+}
+
+function price(value) {
+    console.log(`price of the chai is ${value}`)
+    
+}
+function addTwo(num1,num2){
+    num = num1+num2
+    console.log(num)
+
+}
+printHello();
+price(4111);
+addTwo(3,5)

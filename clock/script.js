616180db49a4fad2bbd933154c7769cf6292b828
amarkatoliya <amarbharavad994@gmail.com>
2025-04-11 16:29:32 +0530

    clock project

---
+++ clock/script.js
@@ -0,0 +1,26 @@
+
+function updateClock(){
+
+const timeElement = document.getElementById('time')
+const dateElement = document.getElementById('date')
+
+const now = new Date();
+const hours = now.getHours() % 12;
+const minutes = now.getMinutes().toString().padStart(2,"0");
+const seconds = now.getSeconds().toString().padStart(2,"0");
+const ampm = now.getHours() >= 12 ? "AM" : "PM";
+
+const options = {
+    weekday: "long",
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+  };
+
+timeElement.textContent = `${hours}:${minutes}:${seconds}:${ampm}`
+dateElement.textContent = now.toLocaleDateString(undefined,options);
+return true ;
+}
+
+setInterval(updateClock,1000)
+updateClock();
